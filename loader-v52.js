e4b79cd5f2dcf58aca9ec7c16854b030f01db87f
EduMarttiins <122230331+EduMarttiins@users.noreply.github.com>
2026-09-13 16:25:09 -0300

    Versão 52: loader do aplicativo

---
+++ loader-v52.js
@@ -0,0 +1,22 @@
+(async()=>{
+  try{
+    const response=await fetch('./index.html?raw=v52&ts='+Date.now(),{cache:'no-store'});
+    if(!response.ok)throw new Error('Falha ao carregar a base do aplicativo');
+    let html=await response.text();
+    html=html.replace(/<meta name="app-version" content="[^"]*">/,'<meta name="app-version" content="52">');
+    html=html.replace(/\.\/manifest\.webmanifest(?:\?[^"']*)?/g,'./manifest.webmanifest?v=52');
+    html=html.replace(/\.\/icons\/lousa-icon\.svg(?:\?[^"']*)?/g,'./icons/lousa-icon-512.png?v=52');
+    html=html.replace(/type="image\/svg\+xml"/g,'type="image/png"');
+    html=html.replace(/<link rel="stylesheet" href="\.\/(?:v3[5-9]|v4[0-9]|v5[0-2]|pwa-v3[9]|pwa-v4[0-9]|pwa-v5[0-2])\.css\?v=\d+">\s*/g,'');
+    html=html.replace(/<script src="\.\/(?:v3[5-9]|v4[0-9]|v5[0-2]|pwa-v3[9]|pwa-v4[0-9]|pwa-v5[0-2])\.js\?v=\d+"(?: defer)?><\/script>\s*/g,'');
+    const css='\n<link rel="stylesheet" href="./v37.css?v=52">\n<link rel="stylesheet" href="./pwa-v39.css?v=52">\n';
+    html=html.replace('</head>',css+'</head>');
+    const scripts='\n<scr'+'ipt src="./v37.js?v=52"></scr'+'ipt>\n<scr'+'ipt src="./v41.js?v=52"></scr'+'ipt>\n<scr'+'ipt src="./v50.js?v=52"></scr'+'ipt>\n<scr'+'ipt src="./pwa-v52.js?v=52"></scr'+'ipt>\n';
+    html=html.replace('</body>',scripts+'</body>');
+    document.open();document.write(html);document.close();
+  }catch(error){
+    const card=document.querySelector('.v52card');
+    if(card)card.innerHTML='<strong>Não foi possível abrir agora.</strong><p>Feche esta aba e abra novamente.</p>';
+    console.error(error);
+  }
+})();
