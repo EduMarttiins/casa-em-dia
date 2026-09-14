0a28ca3aa3da86c3b29c03edca3031834e49f467
EduMarttiins <122230331+EduMarttiins@users.noreply.github.com>
2026-09-13 21:31:46 -0300

    Atualiza service worker para versão 60

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-const VERSION='59-rescue-1';
-const CONTENT_VERSION='59';
+const VERSION='60-rescue-1';
+const CONTENT_VERSION='60';
 const CACHE='lousa-de-estudos-v'+VERSION;
 const ASSETS=[
   './index.html',
   './v52.html',
-  './loader-v55.js?v=59',
-  './v37.css?v=59',
-  './v37.js?v=59',
-  './v41.js?v=59',
-  './v50.js?v=59',
-  './pwa-v39.css?v=59',
-  './pwa-v52.js?v=59',
-  './v54.js?v=59',
-  './v55.js?v=59',
-  './v56.js?v=59',
-  './v57.js?v=59',
-  './v58.js?v=59',
-  './v59.js?v=59',
-  './v55-auto-update.js?v=59',
-  './app-version.json?v=59',
-  './manifest.webmanifest?v=59',
-  './icons/lousa-icon-192.png?v=59',
-  './icons/lousa-icon-512.png?v=59'
+  './loader-v55.js?v=60',
+  './v37.css?v=60',
+  './v37.js?v=60',
+  './v41.js?v=60',
+  './v50.js?v=60',
+  './pwa-v39.css?v=60',
+  './pwa-v52.js?v=60',
+  './v54.js?v=60',
+  './v55.js?v=60',
+  './v56.js?v=60',
+  './v57.js?v=60',
+  './v58.js?v=60',
+  './v59.js?v=60',
+  './v60.js?v=60',
+  './v55-auto-update.js?v=60',
+  './app-version.json?v=60',
+  './manifest.webmanifest?v=60',
+  './icons/lousa-icon-192.png?v=60',
+  './icons/lousa-icon-512.png?v=60'
 ];
 
 self.addEventListener('install',event=>{
