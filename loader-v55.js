8e516ebcbaf868f8dfa472a7c69896a3562d3676
EduMarttiins <122230331+EduMarttiins@users.noreply.github.com>
2026-09-14 08:01:17 -0300

    Versão 66: atualiza carregador e adiciona reparo de inicialização

--- loader-v55.js
+++ loader-v55.js
@@ -1,10 +1,10 @@
 (async()=>{
   try{
-    const response=await fetch('./index.html?raw=v65&ts='+Date.now(),{cache:'no-store'});
+    const response=await fetch('./index.html?raw=v66&ts='+Date.now(),{cache:'no-store'});
     if(!response.ok)throw new Error('Falha ao carregar a base do aplicativo');
     let html=await response.text();
 
-    /* Versão 65: todas as lições liberadas e remoção da faixa explicativa abaixo dos textos. */
+    /* Versão 66: inicialização permanente e reparo das instalações presas em versões antigas. */
     const mcqIds=[];
     const idPattern=/"id"\s*:\s*"([^"]+)"\s*,\s*"type"\s*:\s*"mcq"/g;
     let match;
@@ -25,15 +25,15 @@
     }
 
     html=html.replace(/"type"\s*:\s*"mcq"/g,'"type":"open"');
-    html=html.replace(/<meta name="app-version" content="[^"]*">/,'<meta name="app-version" content="65">');
-    html=html.replace(/\.\/manifest\.webmanifest(?:\?[^"']*)?/g,'./manifest.webmanifest?v=65');
-    html=html.replace(/\.\/icons\/lousa-icon\.svg(?:\?[^"']*)?/g,'./icons/lousa-icon-512.png?v=65');
+    html=html.replace(/<meta name="app-version" content="[^"]*">/,'<meta name="app-version" content="66">');
+    html=html.replace(/\.\/manifest\.webmanifest(?:\?[^"']*)?/g,'./manifest.webmanifest?v=66');
+    html=html.replace(/\.\/icons\/lousa-icon\.svg(?:\?[^"']*)?/g,'./icons/lousa-icon-512.png?v=66');
     html=html.replace(/type="image\/svg\+xml"/g,'type="image/png"');
     html=html.replace(/<link rel="stylesheet" href="\.\/(?:v3[5-9]|v4[0-9]|v5[0-9]|v6[0-9]|pwa-v3[9]|pwa-v4[0-9]|pwa-v5[0-9]|pwa-v6[0-9])\.css\?v=\d+">\s*/g,'');
     html=html.replace(/<script src="\.\/(?:v3[5-9]|v4[0-9]|v5[0-9]|v6[0-9]|pwa-v3[9]|pwa-v4[0-9]|pwa-v5[0-9]|pwa-v6[0-9])\.js\?v=\d+"(?: defer)?><\/script>\s*/g,'');
-    const css='\n<link rel="stylesheet" href="./v37.css?v=65">\n<link rel="stylesheet" href="./pwa-v39.css?v=65">\n';
+    const css='\n<link rel="stylesheet" href="./v37.css?v=66">\n<link rel="stylesheet" href="./pwa-v39.css?v=66">\n';
     html=html.replace('</head>',css+'</head>');
-    const scripts='\n<scr'+'ipt src="./v37.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v41.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v50.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./pwa-v52.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v54.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v55.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v56.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v57.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v58.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v59.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v60.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v62.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v63.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v64.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v65.js?v=65"></scr'+'ipt>\n<scr'+'ipt src="./v55-auto-update.js?v=65"></scr'+'ipt>\n';
+    const scripts='\n<scr'+'ipt src="./v37.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v41.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v50.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./pwa-v52.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v54.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v55.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v56.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v57.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v58.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v59.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v60.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v62.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v63.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v64.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v65.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v66.js?v=66"></scr'+'ipt>\n<scr'+'ipt src="./v55-auto-update.js?v=66"></scr'+'ipt>\n';
     html=html.replace('</body>',scripts+'</body>');
     document.open();document.write(html);document.close();
   }catch(error){
