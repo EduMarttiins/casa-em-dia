78f2e40810c95473d104d7f37b632760a51b1aa1
EduMarttiins <122230331+EduMarttiins@users.noreply.github.com>
2026-09-14 11:00:41 -0300

    Renova carregador seguro da versão 68

--- loader-v68.js
+++ loader-v68.js
@@ -1,10 +1,9 @@
 (async()=>{
   try{
-    const response=await fetch('./index.html?raw=v68fix3&ts='+Date.now(),{cache:'no-store'});
+    const response=await fetch('./index.html?raw=v68fix4&ts='+Date.now(),{cache:'no-store'});
     if(!response.ok)throw new Error('Falha ao carregar a base do aplicativo');
     let html=await response.text();
 
-    /* Versão 68: Português preserva o formato original de cada questão, aberta ou múltipla escolha. */
     const mcqIds=[];
     const idPattern=/"id"\s*:\s*"([^"]+)"\s*,\s*"type"\s*:\s*"mcq"/g;
     let match;
@@ -25,14 +24,14 @@
     }
 
     html=html.replace(/<meta name="app-version" content="[^"]*">/,'<meta name="app-version" content="68">');
-    html=html.replace(/\.\/manifest\.webmanifest(?:\?[^"']*)?/g,'./manifest.webmanifest?v=68fix3');
-    html=html.replace(/\.\/icons\/lousa-icon\.svg(?:\?[^"']*)?/g,'./icons/lousa-icon-512.png?v=68fix3');
+    html=html.replace(/\.\/manifest\.webmanifest(?:\?[^"']*)?/g,'./manifest.webmanifest?v=68fix4');
+    html=html.replace(/\.\/icons\/lousa-icon\.svg(?:\?[^"']*)?/g,'./icons/lousa-icon-512.png?v=68fix4');
     html=html.replace(/type="image\/svg\+xml"/g,'type="image/png"');
     html=html.replace(/<link rel="stylesheet" href="\.\/(?:v3[5-9]|v4[0-9]|v5[0-9]|v6[0-9]|pwa-v3[9]|pwa-v4[0-9]|pwa-v5[0-9]|pwa-v6[0-9])\.css\?v=[^"']+">\s*/g,'');
     html=html.replace(/<script src="\.\/(?:v3[5-9]|v4[0-9]|v5[0-9]|v6[0-9]|pwa-v3[9]|pwa-v4[0-9]|pwa-v5[0-9]|pwa-v6[0-9])\.js\?v=[^"']+"(?: defer)?><\/script>\s*/g,'');
-    const css='\n<link rel="stylesheet" href="./v37.css?v=68fix3">\n<link rel="stylesheet" href="./pwa-v39.css?v=68fix3">\n';
+    const css='\n<link rel="stylesheet" href="./v37.css?v=68fix4">\n<link rel="stylesheet" href="./pwa-v39.css?v=68fix4">\n';
     html=html.replace('</head>',css+'</head>');
-    const scripts='\n<scr'+'ipt src="./v37.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v41.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v50.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./pwa-v52.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v54.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt>window.__lousaCurrentContentVersion=68;try{window.__lousaV68PortugueseSnapshot=JSON.parse(JSON.stringify(portugueseLessons));}catch(e){window.__lousaV68PortugueseSnapshot=[];}</scr'+'ipt>\n<scr'+'ipt src="./v55.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v56.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v57.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v58.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v59.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v60.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v62.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v63.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v64.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v65.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v66.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v67.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v68.js?v=68fix3"></scr'+'ipt>\n<scr'+'ipt src="./v55-auto-update.js?v=68fix3"></scr'+'ipt>\n';
+    const scripts='\n<scr'+'ipt src="./v37.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v41.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v50.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./pwa-v52.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v54.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt>window.__lousaCurrentContentVersion=68;try{window.__lousaV68PortugueseSnapshot=JSON.parse(JSON.stringify(portugueseLessons));}catch(e){window.__lousaV68PortugueseSnapshot=[];}</scr'+'ipt>\n<scr'+'ipt src="./v55.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v56.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v57.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v58.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v59.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v60.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v62.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v63.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v64.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v65.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v66.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v67.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v68.js?v=68fix4"></scr'+'ipt>\n<scr'+'ipt src="./v55-auto-update.js?v=68fix4"></scr'+'ipt>\n';
     html=html.replace('</body>',scripts+'</body>');
     document.open();document.write(html);document.close();
   }catch(error){
